ced866cc2339a4a480bb9dc18159f63fe74127bb
Anteneh Tefera <kibrat2003@yahoo.com>
2022-12-23 23:02:53 +0000

    fact table update

--- models/fact/fact_sales.sql
+++ models/fact/fact_sales.sql
@@ -3,17 +3,19 @@ with stg_sales as(
   select *
   from  {{ref('staging_sales')}} 
 ),
---select * from test
+--select * from stg_sales
 
- dim_sales as(
-  select *
  
+  dim_sales as(
+  select *
  from  {{ref('dim_sales')}} 
 ),
 
+--select * from dim_sales
+
 sales_fact as (
 select * from stg_sales a
-left outer join  dim_sales b on a.sales_personname=b.sales_person_name
+left outer join  dim_sales b on a.sales_personname=b.salespersonname
 --left outer join test2 b on a.sales_personname=b.sales_person_name
 )
 select * from sales_fact
