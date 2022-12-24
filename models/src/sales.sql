6cff71fa15aeb8ec76972ed69e21f61f01b9ddd9
Anteneh Tefera <kibrat2003@yahoo.com>
2022-12-24 18:36:56 +0000

    save

--- models/src/sales.sql
+++ models/src/sales.sql
@@ -1,20 +1,5 @@
-WITH sales_listing
-AS (
-	SELECT *
-/*		,CASE 
-			WHEN product_name IS NULL
-				OR product_name = ''
-				THEN 'Produsct Name Not Found'
-			ELSE product_name
-			END AS productnamedesc
-		,CASE 
-			WHEN sales_personname IS NULL
-				OR sales_personname = ''
-				THEN 'Person Name Not Found'
-			ELSE sales_personname
-			END AS salespersonnamedesc
-*/
-	FROM DBT_DEMO.DEMO.sales
-	)
-SELECT *
-FROM sales_listing
+with sales_listing as (
+    select * 
+    from dbt_demo.demo.sales
+    ) 
+    select * from sales_listing
