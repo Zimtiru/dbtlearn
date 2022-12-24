2d8f6db19db0f11888c44b89815afe603c16a291
Anteneh Tefera <kibrat2003@yahoo.com>
2022-12-24 18:38:54 +0000

    update column name

--- models/staging/staging_sales.sql
+++ models/staging/staging_sales.sql
@@ -12,13 +12,13 @@ AS (
 				OR product_name = ''
 				THEN 'Produsct Name Not Found'
 			ELSE product_name
-			END AS product_name_desc
+			END AS productnamedesc
 		,CASE 
 			WHEN sales_personname IS NULL
 				OR sales_personname = ''
 				THEN 'Person Name Not Found'
 			ELSE sales_personname
-			END AS sales_person_name_desc
+			END AS salespersonnamedesc
 	FROM  {{ref('sales')}}
 	)
 SELECT *
