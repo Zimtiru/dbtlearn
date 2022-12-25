0b4bc02d67355e99a1549710b7c0e1c59b76ce1b
Anteneh Tefera <kibrat2003@yahoo.com>
2022-12-25 22:31:35 +0000

    use of ref

--- models/src/dim_sales.sql
+++ models/src/dim_sales.sql
@@ -2,7 +2,8 @@
 
 with
     sales_product_listing as (
-        select salespersonid, salespersonname from dbt_demo.demo.dim_sales_product
+        select salespersonid, salespersonname from  {{ref('dim_sales_product')}}
+        
     )
 select *
 from sales_product_listing
