04453a92bd8926400135aa714d25a4b005073ce1
Anteneh Tefera <kibrat2003@yahoo.com>
2022-12-25 01:03:06 +0000

    add one cte staging for fact table

--- models/fact/fact_sales.sql
+++ models/fact/fact_sales.sql
@@ -5,12 +5,13 @@ with
 
     dim_product as (select productid, productname from {{ ref("dim_product") }}),
     -- select * from dim_product
-    sales_fact as (
+    sales_fact_1 as (
         select *
         from stg_sales a
         inner join dim_sales b on a.sales_personname = b.salespersonname
         inner join dim_product p on a.product_name = p.productname
-    )
+    ),
+fact_sales_2 as (
 select  
     to_date(salesdate) as salesdate,
     salespersonid,
@@ -18,6 +19,9 @@ select
     sum(soldqty) as totalsoldqty,
     productnamedesc,
     salespersonnamedesc
-from sales_fact
+from sales_fact_1
 group by salesdate, salespersonid, productid, productnamedesc, salespersonnamedesc
 order by salesdate desc
+)
+
+select * from fact_sales_2
