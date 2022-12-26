51157bf6c6fb8f704014c370dc0b29bf993cfbfe
Anteneh Tefera <kibrat2003@yahoo.com>
2022-12-26 19:04:47 +0000

    Update formate

--- models/fact/sales_fact.sql
+++ models/fact/sales_fact.sql
@@ -1,29 +1,21 @@
 with
-    staging_sales as (
-            select * from {{ ref("stg_sales_listing") }}
-            ),
---    select * from  staging_sales
-    dim_sales as (
-        select * from {{ ref("dim_sales") }}
-        ),
+    staging_sales as (select * from {{ ref("stg_sales_listing") }}),
+    -- select * from  staging_sales
+    dim_sales as (select * from {{ ref("dim_sales") }}),
 
--- select * from  dim_sales
-    dim_product as (
-        select * from {{ ref("dim_product") }}
-    ),
+    -- select * from  dim_sales
+    dim_product as (select * from {{ ref("dim_product") }}),
 
-final as (
-        select 
+    final as (
+        select
             a.salesdate,
             c.productid,
             b.salespersonid,
-            a.SALESPERSONNAMEDESC,
-            a.PRODUCTNAMEDESC
+            a.salespersonnamedesc,
+            a.productnamedesc
         from staging_sales a
-        inner join  dim_sales    b on  upper(a.salespersonname)= upper(b.salespersonname)
-        inner join  dim_product  c on  upper(a.productname)= upper(c.productname)
+        inner join dim_sales b on upper(a.salespersonname) = upper(b.salespersonname)
+        inner join dim_product c on upper(a.productname) = upper(c.productname)
     )
-        select * from final
-
-
-
+select *
+from final
