6b42cb850561d80596417ff26367b99a1cd03ade
Anteneh Tefera <kibrat2003@yahoo.com>
2022-12-26 17:55:57 +0000

    save

--- models/fact/sales_fact.sql
+++ models/fact/sales_fact.sql
@@ -2,7 +2,7 @@ with
     staging_sales as (
             select * from {{ ref("stg_sales_listing") }}
             ),
-    -- select * from  staging_sales
+--    select * from  staging_sales
     dim_sales as (
         select * from {{ ref("dim_sales") }}
         ),
@@ -13,14 +13,17 @@ with
     ),
 
 final as (
-        select a.salesdate,
+        select 
+            a.salesdate,
             c.productid,
             b.salespersonid,
-            a.productnamedesc,
-            a.salespersondesc
+            a.SALESPERSONNAMEDESC,
+            a.PRODUCTNAMEDESC
         from staging_sales a
         inner join  dim_sales    b on  upper(a.salespersonname)= upper(b.salespersonname)
         inner join  dim_product  c on  upper(a.productname)= upper(c.productname)
     )
         select * from final
+
+
 
