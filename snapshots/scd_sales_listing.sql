9ecc9533a9e925f05b70ec1b3a4986224f6c187a
Anteneh Tefera <kibrat2003@yahoo.com>
2023-01-02 15:18:35 +0000

    update

--- snapshots/scd_sales_listing.sql
+++ snapshots/scd_sales_listing.sql
@@ -1,11 +1,11 @@
 {% snapshot scd_sales_fact_snapshot %}
 {{
     config(
-        target_database='DBT_DEMO',
-        target_schema='DBT_ATEFERA',
-        unique_key='id',
-        strategy='timestamp',
-        updated_at='updated_at',
+        target_database ='DBT_DEMO',
+        target_schema   ='DBT_ATEFERA',
+        unique_key      ='id',
+        strategy        ='timestamp',
+        updated_at      ='updated_at',
         invalidate_hard_deletes=True,
     )
 }} 
