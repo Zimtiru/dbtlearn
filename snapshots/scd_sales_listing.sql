64400f2089d7f84b5606d6d7c94827ccee678f6c
Anteneh Tefera <kibrat2003@yahoo.com>
2023-01-02 16:06:22 +0000

    save

--- snapshots/scd_sales_listing.sql
+++ snapshots/scd_sales_listing.sql
@@ -3,6 +3,7 @@
     config(
         target_database ='DBT_DEMO',
         target_schema   ='DBT_ATEFERA',
+        unique_key      ='id',
         strategy        ='timestamp',
         updated_at      ='updated_at',
         invalidate_hard_deletes=True,
