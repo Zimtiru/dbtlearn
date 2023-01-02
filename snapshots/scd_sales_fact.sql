de06c80dbe8d832d195d122b8f81ac0a9c012d0d
Anteneh Tefera <kibrat2003@yahoo.com>
2023-01-02 20:23:11 +0000

    save

---
+++ snapshots/scd_sales_fact.sql
@@ -0,0 +1,15 @@
+{% snapshot scd_sales_fact%}
+{{
+    config(
+        target_database ='DBT_DEMO',
+        target_schema   ='DBT_TEFERA',
+        unique_key      ='id',
+        strategy        ='timestamp',
+        updated_at      ='updated_at',
+        invalidate_hard_deletes=True,
+    )
+}} 
+
+select * from {{ source('DBT_DEMO', 'sales_source') }}
+
+{% endsnapshot %}
