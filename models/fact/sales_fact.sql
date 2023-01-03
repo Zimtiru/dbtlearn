8424a7d5d7ffa63dea8b9680d871cdbdba3f8220
Anteneh Tefera <kibrat2003@yahoo.com>
2023-01-03 17:23:14 +0000

    save

--- models/fact/sales_fact.sql
+++ models/fact/sales_fact.sql
@@ -1,12 +1,12 @@
 
 
-with staging_sales as (select * from {{ ref("stg_sales_listing") }}),
+with staging_sales as (select * from {{ ref("stg_sales_listing") }})
 
-    --select * from  staging_sales
+    select * from  staging_sales
 
-    dim_sales as (select * from {{ ref("dim_sales") }}),
+    with dim_sales as (select * from {{ ref("dim_sales") }})
 
-    -- select * from  dim_sales
+    select * from  dim_sales
     dim_product as (select * from {{ ref("dim_product") }}),
 
     final as (
@@ -14,15 +14,13 @@ with staging_sales as (select * from {{ ref("stg_sales_listing") }}),
             a.salesdate,
             c.productid,
             b.salespersonid,
-        --    a.salespersonnamedesc,
-         --   a.productnamedesc,
             a.validationdesc,
             sum(a.soldqty) as TotalSoldqty
         from staging_sales a
-        inner join dim_sales b   on upper(a.salespersonname_new) = upper(b.salespersonname_new)
-        inner join dim_product c on upper(a.productname_new)     = upper(c.productname_new)
+        inner join dim_sales b   on upper(a.salespersonname_new) = upper(b.salespersonname)
+        inner join dim_product c on upper(a.productname_new)     = upper(c.productname)
         group by 1,2,3,4
     )
 select distinct
-    date(salesdate) as salesdate, productid, salespersonid, TotalSoldqty, salespersonnamedesc, productnamedesc,validationdesc
+    date(salesdate) as salesdate, productid, salespersonid, TotalSoldqty,validationdesc
 from final
--- models/staging/stg_sales_listing.sql
+++ models/staging/stg_sales_listing.sql
@@ -3,13 +3,12 @@
 with
     stg_sales_listing_first as (
         select
-            *,
-            case
-                when productname = '' then null else productname
-            end as productname_new,
+            salesdate,
+            soldqty,
+            case when productname = '' then null else productname end as productname,
             case
                 when salespersonname = '' then null else salespersonname
-            end as salespersonname_new
+            end as salespersonname
         from {{ ref("sales_source") }}
     ),
     -- select * from  stg_sales_listing_first
@@ -17,11 +16,11 @@ with
         select
             *,
             case
-                when productname_new is null and salespersonname_new is not null
+                when productname is null and salespersonname is not null
                 then 'Product Name Not Found'
-                when productname_new is not null and salespersonname_new is null
+                when productname is not null and salespersonname is null
                 then 'Person Name Not Found'
-                when (productname_new is null) and (salespersonname_new is null)
+                when (productname is null) and (salespersonname is null)
                 then 'Person Name Not Found, Product Name Not Found'
             -- else    productname or productname
             end as validationdesc
