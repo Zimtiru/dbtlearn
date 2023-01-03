4ffff3f777d0a5f82107cc0483d6fdab9a7d94ed
Anteneh Tefera <kibrat2003@yahoo.com>
2023-01-03 21:14:22 +0000

    save

--- models/fact/sales_fact.sql
+++ models/fact/sales_fact.sql
@@ -2,14 +2,14 @@ with
     staging_sales as (
         select salesdate, soldqty, productname, salespersonname, validationdesc
         from {{ ref("stg_sales_listing") }}
-    )
-     select * from  staging_sales
+    ),
+    -- select * from  staging_sales
     dim_sales as (select * from {{ ref("dim_sales") }}),
 
     -- select * from  dim_sales
     dim_product as (select * from {{ ref("dim_product") }}),
 
-    final as (
+    final_stg as (
         select
             a.salesdate,
             c.productid,
@@ -21,8 +21,19 @@ with
             dim_sales b on upper(a.salespersonname) = upper(b.salespersonname)
         left outer join dim_product c on upper(a.productname) = upper(c.productname)
         group by 1, 2, 3, 4
+    ),
+
+
+    final as (
+        select
+            salesdate,
+            productid,
+            salespersonid,
+            totalsoldqty,
+            validationdesc,
+            case when validationdesc is null then 1 else 2 end as processstatusid
+        from final_stg
     )
-select distinct
-    date(salesdate) as salesdate, productid, salespersonid, totalsoldqty, validationdesc
+select *
 from final
-order by salesdate desc
+where processstatusid = 1
