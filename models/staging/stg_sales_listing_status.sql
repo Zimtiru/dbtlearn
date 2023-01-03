9842237e483df1afc9fc7a26dccc75250ee804a9
Anteneh Tefera <kibrat2003@yahoo.com>
2023-01-03 21:12:33 +0000

    save

---
+++ models/staging/stg_sales_listing_status.sql
@@ -0,0 +1,28 @@
+with
+    staging_sales as (
+        select salesdate, soldqty, productname, salespersonname, validationdesc
+        from {{ ref("stg_sales_listing") }}
+    ),
+    -- select * from  staging_sales
+    dim_sales as (select * from {{ ref("dim_sales") }}),
+
+    -- select * from  dim_sales
+    dim_product as (select * from {{ ref("dim_product") }}),
+
+    final as (
+        select
+            a.salesdate,
+            c.productid,
+            b.salespersonid,
+            a.validationdesc,
+            sum(a.soldqty) as totalsoldqty
+        from staging_sales a
+        left outer join
+            dim_sales b on upper(a.salespersonname) = upper(b.salespersonname)
+        left outer join dim_product c on upper(a.productname) = upper(c.productname)
+        group by 1, 2, 3, 4
+    )
+select distinct
+    date(salesdate) as salesdate, productid, salespersonid, totalsoldqty, validationdesc
+from final
+order by productid
