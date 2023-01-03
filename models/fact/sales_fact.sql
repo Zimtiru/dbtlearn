30609c3a5d693cf1684338f5fb0091ee7315c22e
Anteneh Tefera <kibrat2003@yahoo.com>
2023-01-03 20:57:17 +0000

    save

--- models/fact/sales_fact.sql
+++ models/fact/sales_fact.sql
@@ -1,12 +1,12 @@
+with
+    staging_sales as (
+        select salesdate, soldqty, productname, salespersonname, validationdesc
+        from {{ ref("stg_sales_listing") }}
+    )
+     select * from  staging_sales
+    dim_sales as (select * from {{ ref("dim_sales") }}),
 
-
-with staging_sales as (
-    select  salesdate, soldqty,productname,salespersonname,validationdesc from {{ ref("stg_sales_listing") }}),
-    --select * from  staging_sales
-
-     dim_sales as (select * from {{ ref("dim_sales") }}),
-
-    --select * from  dim_sales
+    -- select * from  dim_sales
     dim_product as (select * from {{ ref("dim_product") }}),
 
     final as (
@@ -15,13 +15,14 @@ with staging_sales as (
             c.productid,
             b.salespersonid,
             a.validationdesc,
-            sum(a.soldqty) as TotalSoldqty
+            sum(a.soldqty) as totalsoldqty
         from staging_sales a
-        left outer join dim_sales b   on upper(a.salespersonname) = upper(b.salespersonname)
-        left outer join dim_product c on upper(a.productname)     = upper(c.productname)
-        group by 1,2,3,4
+        left outer join
+            dim_sales b on upper(a.salespersonname) = upper(b.salespersonname)
+        left outer join dim_product c on upper(a.productname) = upper(c.productname)
+        group by 1, 2, 3, 4
     )
 select distinct
-    date(salesdate) as salesdate, productid, salespersonid, TotalSoldqty,validationdesc
+    date(salesdate) as salesdate, productid, salespersonid, totalsoldqty, validationdesc
 from final
-order by productid
+order by salesdate desc
