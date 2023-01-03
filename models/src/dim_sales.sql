320dc9bffc5d3ac7cc95bddaf4b3c0527086afc5
Anteneh Tefera <kibrat2003@yahoo.com>
2023-01-03 22:29:25 +0000

    save

--- models/src/dim_sales.sql
+++ models/src/dim_sales.sql
@@ -1,6 +1,6 @@
 with
     dim_saleslisting as (
-        select salespersonid, salespersonname from  DBT_DEMO.DEMO.DIM_SALES_PRODUCT
+         select salespersonid, salespersonname from  DBT_DEMO.DEMO.DIM_SALES
         
     )
 select *
